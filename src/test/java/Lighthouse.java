01c67dd011df20ce9fe8c90b6ea9dc359b98a7e1
krish.t <krish.t>
2019-12-13 12:03:07 +0530

    Lighthouse : Mobile +Desktop

--- src/test/java/Lighthouse.java
+++ src/test/java/Lighthouse.java
@@ -1,6 +1,10 @@
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.MultiPartEmail;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,7 +20,9 @@ import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -24,8 +30,9 @@ public class Lighthouse {
     WebDriver driver;
     ExtentReports extent;
     ExtentTest logger;
-    String mode ="desktop";
-    WebDriverWait wait ;
+    List<String> mode =new ArrayList<>();
+    WebDriverWait wait;
+    int i =0;
     @BeforeTest
                 public void setUp() {
 //            Driver Paths and Setting properties
@@ -48,9 +55,10 @@ public class Lighthouse {
             logs.enable(LogType.PERFORMANCE, Level.ALL);
             caps.setCapability("goog:loggingPrefs", logs );
             caps.setCapability(CapabilityType.LOGGING_PREFS, logs);
-
+            mode.add("mobile");
+            mode.add("desktop");
         }
-    @Test
+    @Test(invocationCount = 2)
     public void lightHouse() throws Exception{
         Set<String> list = new HashSet<>();
         File file = new File("C:\\Users\\krish.t\\Desktop\\quiz\\resources\\LighthouseURLs.txt");
@@ -60,12 +68,14 @@ public class Lighthouse {
             list.add(s);
         System.out.println(list.size()+" "+list);
             for(String link: list) {
+                try{
                 if (link != null) {
-                    logger = extent.startTest("Lighthouse : " + link);
+                    String type = mode.get(i);
+                    logger = extent.startTest(type +" : Lighthouse : " + link);
                     System.out.println(link + "\n*************************************************************************");
                     driver = new ChromeDriver();
-                    wait = new WebDriverWait(driver, 30);
-                    String testURL = "https://googlechrome.github.io/lighthouse/viewer/?psiurl=" + link + "&strategy=" + mode + "&category=performance&category=accessibility&category=best-practices&category=seo&utm_source=lh-chrome-ext";
+                    wait = new WebDriverWait(driver, 300);
+                    String testURL = "https://googlechrome.github.io/lighthouse/viewer/?psiurl=" + link + "&strategy=" + type + "&category=performance&category=accessibility&category=best-practices&category=seo&utm_source=lh-chrome-ext";
                     driver.get(testURL);
                     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='lh-header-container']//a[1]//div[2]")));
                     System.out.println(driver.findElement(By.className("lh-gauge__percentage")).getText());
@@ -78,23 +88,48 @@ public class Lighthouse {
                         System.out.println(category);
 
                         if (Integer.parseInt(score) < 40)
-                            logger.log(LogStatus.FAIL, category + " : " + score);
+                            logger.log(LogStatus.FATAL, category + " : " + score);
                         else if (Integer.parseInt(score) < 60)
-                            logger.log(LogStatus.WARNING, category + " : " + score);
+                            logger.log(LogStatus.FAIL, category + " : " + score);
                         else
                             logger.log(LogStatus.PASS, category + " : " + score);
 
                     }
                     driver.quit();
                     extent.endTest(logger);
-                }
+                }}catch (Exception e){logger.log(LogStatus.SKIP,"Timed out while auditing the URL : "+link);
+                driver.quit();
+                extent.endTest(logger);}
             }
+        i++;
     }
     @AfterTest
     public void closeTest(){
        driver.quit();
        extent.endTest(logger);
        extent.flush();
+       try {
+           sendEmail();
+       }catch (Exception e){}
        extent.close();
+    }
+
+    public void sendEmail() throws EmailException {
+        EmailAttachment attachment = new EmailAttachment();
+        attachment.setPath(System.getProperty("user.dir")+"\\Lighthouse.html");
+        attachment.setDisposition(EmailAttachment.ATTACHMENT);
+        String to []={"krish.t@media.net","saili.m@media.net","fenil.g@media.net"};
+
+        MultiPartEmail email = new MultiPartEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(587);
+        email.setAuthenticator(new DefaultAuthenticator("krish.t@media.net", "fstomkzuhqlyvcuv"));
+        email.setSSLOnConnect(true);
+        email.addTo(to);
+        email.setFrom("krish.t@media.net", "Krish");
+        email.setSubject("Lighthouse Test - Automation Report");
+        email.setMsg("PFA The below Lighthouse Audit Report");
+        email.attach(attachment);
+        email.send();
     }
 }
